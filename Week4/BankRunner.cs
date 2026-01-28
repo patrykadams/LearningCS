2bf34eeff24749b1519d2f1163925f4d74436442
Patryk Adamski <patrykadamski123@gmail.com>
2026-01-28 23:30:18 +0100

    Week 4: Added Exception Handling and input validation

---
+++ Week4/BankRunner.cs
@@ -0,0 +1,30 @@
+﻿// File: BankRunner.cs
+// Role: The main entry point for the CSharp_Masterclass application.
+using System;
+using Week2_OOP;
+
+namespace CSharp_Masterclass
+{
+    class BankRunner
+    {
+                static void Main(string[] args)
+        {
+            Console.WriteLine("=== Automated Banking System Runner ===");
+
+            try
+            {
+                RunApp();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Critical Error: {ex.Message}");
+            }
+        }
+
+        static void RunApp()
+        {
+            BankAccount account = new BankAccount("Patryk", 1000m);
+            account.DisplayBalance();
+        }
+    }
+}
